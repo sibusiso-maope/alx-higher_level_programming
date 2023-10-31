e1740542bfa1bb7f79ab9c8373bb68e1ca50e5f7
sibusiso-maope <sibusiso.maope@gmail.com>
2023-10-31 12:59:04 -0700

    My last digit task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-def to_uper(character):
-    if ord(character) >= 97 and ord(character) <= 122:
-        return (ord(character) - 32)
+def to_uper(char):
+    if ord(char) >= 97 and ord(char) <= 122:
+        return (ord(char) - 32)
     else:
-        return ord(character)
+        return ord(char)
 
 def uppercase(string):
     str_new = ""
-    for character in string:
-        str_new += "%c" % to_uper(character)
+    for char in string:
+        str_new += "%c" % to_uper(char)
     print("{:s}".format(str_new))
