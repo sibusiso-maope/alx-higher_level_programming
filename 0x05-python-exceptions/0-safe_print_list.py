b99336d71353bf754976a8829fdf717194f9c020
sibusiso-maope <sibusiso.maope@gmail.com>
2023-11-20 10:38:33 -0800

    tried task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
+    res = 0
     for k in range(x):
         try:
             print(my_list[k], end="")
-            num += 1
+            res += 1
         except IndexError:
             break
     print("")
-    return(num)
+    return(res)
