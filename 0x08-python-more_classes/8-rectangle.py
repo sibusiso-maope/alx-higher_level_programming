e9beab5e2c85994cb1bd0c3e428eae748fa20369
sibusiso-maope <sibusiso.maope@gmail.com>
2023-11-27 03:20:24 -0800

    My task 8 fix

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -87,9 +87,9 @@ class Rectangle:
             return ("")
 
         rect = []
-        for k in range(self.__height):
-            [rect.append(str(self.print_symbol)) for l in range(self.__width)]
-            if k != self.__height - 1:
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
 
